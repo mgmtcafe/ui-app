b59939241f3a86b120738f8abc3c4eadf57253ec
Ronjoy Barthakur <mgmtcafe@gmail.com>
2018-04-02 17:28:59 +0530

    Ronjoy UI

---
+++ src/main/java/com/gateway/UiApplication.java
@@ -0,0 +1,17 @@
+package com.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableOAuth2Sso
+public class UiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiApplication.class, args);
+	}
+}
